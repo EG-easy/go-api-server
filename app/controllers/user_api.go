c76afda6f5c8d7681d4ad42dc07fdb71df1c1d6e
EG-easy <1018eguchi@gmail.com>
2018-12-17 16:12:10 +0900

    add new api function

--- app/controllers/user_api.go
+++ app/controllers/user_api.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/revel/revel"
 	"go-api-server/app/models"
+	"strconv"
 )
 
 type UserApi struct {
@@ -21,6 +22,21 @@ func (c UserApi) GetUsers() revel.Result {
 
 	response := JsonResponse{}
 	response.Response = Users
+
+	return c.RenderJSON(response)
+}
+
+func (c UserApi) GetUser() revel.Result {
+
+	//int型に変換する
+	number , _ := strconv.Atoi(c.Params.Get("number"))
+
+	Users := []models.User{}
+
+	DB.Find(&Users)
+
+	response := JsonResponse{}
+	response.Response = Users[number]
 
 	return c.RenderJSON(response)
 }
